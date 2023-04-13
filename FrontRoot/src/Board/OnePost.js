558d377caf1cbf8c0e31315ce37dd9a4d59696cc
SHM <ajdwk91@naver.com>
2023-04-14 02:21:54 +0900

    잘못 커밋 수정

--- FrontRoot/src/Board/OnePost.js
+++ FrontRoot/src/Board/OnePost.js
@@ -35,7 +35,7 @@ function OnePost(props) {
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    return `${year}-${month}-${day}\n${hours}:${minutes}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
   };
 
   return (
